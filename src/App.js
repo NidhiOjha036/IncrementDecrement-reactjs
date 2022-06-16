be3f5b48bd6924315e0c11ffb7b11e7b7ca997c8
Nidhi Ojha <ojhanidhi036@gmail.com>
2022-06-16 18:08:20 +0530

    all files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [num, setNum] = useState(0)
+  useEffect(() => {console.log("Hello")},[num] )
+
+  const incNum = () =>{
+    setNum(num+1)
+  }
+
+  const decNum = () =>{
+    if(num>0){
+      setNum(num-1)
+    }else{
+      alert('Sorry, Zero Limit Reached');
+      setNum(0);
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main_div'>
+        <div className='center_div'>
+          <h1> {num} </h1>
+          <div className='btn_div'>
+            <button onClick={incNum}>Increment</button>
+            <button onClick={decNum}>Decrement</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
